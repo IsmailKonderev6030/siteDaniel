5eff00fdbb05d86daed8b4dc937db884e191bac7
IsmailKonderev6030 <ismailkonderev6030@gmail.com>
2024-12-30 21:02:28 +0300

    added valid number enter

--- forDaniel/log-in/JS/index.js
+++ forDaniel/log-in/JS/index.js
@@ -1,1 +1,6 @@
-fsd
+let elementPhoneInput = document.getElementById("phoneInput");
+let maskOptionPhone = {
+    mask: '+{7}(000)000-00-00',
+    lazy: true
+}
+let maskPhone = new IMask(elementPhoneInput,maskOptionPhone);
